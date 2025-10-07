22c1915c662b39066c48716ca0d49c1062bf7a74
Minh Huy Tran <huy@perun.network>
2025-10-07 15:34:35 +1100

    add log
    Signed-off-by: Minh Huy Tran <huy@perun.network>

--- wallet/backend.go
+++ wallet/backend.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"io"
+	"log"
 
 	"github.com/pkg/errors"
 
@@ -42,6 +43,8 @@ func (b backend) VerifySignature(msg []byte, sig wallet.Sig, a wallet.Address) (
 	hash := crypto.Keccak256(msg)
 	prefix := []byte("\x19Ethereum Signed Message:\n32")
 	hash = crypto.Keccak256(prefix, hash)
+
+	log.Println("Verifying hash:", hash)
 	sigCopy := make([]byte, 65) //nolint:gomnd
 	copy(sigCopy, sig)
 	if len(sigCopy) == 65 && (sigCopy[65-1] >= 27) { //nolint:gomnd
